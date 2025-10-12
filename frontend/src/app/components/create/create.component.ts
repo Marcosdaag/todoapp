1f967f9c00458cb196f3e9b482848e42f418dfeb
Marcosdaag <marcosoffs99@gmail.com>
2025-10-11 22:58:21 -0300

    create

--- frontend/src/app/components/create/create.component.ts
+++ frontend/src/app/components/create/create.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Pending } from '../../models/pending.model';
+import { PendingService } from '../../services/pending.service';
 
 @Component({
   selector: 'app-create',
   standalone: false,
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
+  providers: [PendingService]
 })
 export class CreateComponent {
+  public tittle: string;
+  public pending: Pending;
+
+  constructor(
+    private _pendingService: PendingService
+  ){
+    this.tittle = "Create new pending";
+    this.pending = new Pending('','',false);
+  }
 
 }
