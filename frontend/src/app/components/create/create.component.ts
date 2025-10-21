f32b816146916a66d4f67053b5fa3885ce397939
Marcosdaag <marcosoffs99@gmail.com>
2025-10-21 09:05:22 -0300

    validacion y error message

--- frontend/src/app/components/create/create.component.ts
+++ frontend/src/app/components/create/create.component.ts
@@ -13,23 +13,34 @@ import { NgForm } from '@angular/forms';
 export class CreateComponent {
   public tittle: string;
   public pending: Pending;
+  showError: boolean = false;
 
   constructor(
     private _pendingService: PendingService
-  ){
+  ) {
     this.tittle = "Create new pending";
-    this.pending = new Pending('','New Pending',false);
+    this.pending = new Pending('', '', false);
   }
 
 
-  onSubmit(formulario: NgForm){
+  onSubmit(formulario: NgForm) {
+    // Si el formulario no le da un valor a name no puede ser enviado
+    if (!this.pending.name) {
+      return;
+    }
+    
+    // Primer letra el del pending en mayuscula
+    this.pending.name = this.pending.name.charAt(0).toUpperCase() + this.pending.name.slice(1);
+
+    // Hace un create del pending, llamando al meotodo save pending que esta en el servicio y resetea tanto el formulario como el nombre del pendiente por default
     this._pendingService.savePending(this.pending).subscribe(
-      response =>{
+      response => {
         console.log(response);
         formulario.reset();
+        this.pending.name = '';
       },
-      error =>{
-        console.log(<any>error)
+      error => {
+        console.log(<any>error);
       }
     );
   }
