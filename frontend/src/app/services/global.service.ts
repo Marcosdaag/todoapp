f3ea830649473f015250737ca1eb77fea7ee4297
Marcosdaag <marcosoffs99@gmail.com>
2025-11-17 16:47:22 -0300

    url fix

--- frontend/src/app/services/global.service.ts
+++ frontend/src/app/services/global.service.ts
@@ -1,6 +1,6 @@
 // Configuracion global y variables que voy a estar reutilizando
 
 export var Global ={
-    url: 'https://todoappapi-gs5y.onrender.com'
+    url: 'https://todoappapi-gs5y.onrender.com/'
 };
 
